c1751e64437bcac7e0426eff978db9841478b4ee
Kittima.M <kittima.meesrisong@hotmail.com>
2024-07-27 15:38:06 +0700

    Apply server side validation with spring-boot-starter-validation

--- src/main/java/web/spring/school/controller/ContactController.java
+++ src/main/java/web/spring/school/controller/ContactController.java
@@ -1,8 +1,12 @@
 package web.spring.school.controller;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,14 +27,19 @@ public class ContactController {
 
 
     @RequestMapping("/contact")
-    public String displayContactPage() {
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact", new Contact());
         return "contact.html";
 
     }
 
     @PostMapping("/saveMsg")
-    public ModelAndView saveMessage(@RequestParam Contact contact){
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors){
+        if(errors.hasErrors()){
+            log.error("Contact form validation failed due to :{}", errors.toString());
+            return "contact.html";
+        }
         contactService.saveMessageDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 }
--- src/main/java/web/spring/school/model/Contact.java
+++ src/main/java/web/spring/school/model/Contact.java
@@ -1,14 +1,30 @@
 package web.spring.school.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Contact {
 
+    @NotBlank(message = "Name must not be blank")
+    @Size(min=3, message = "Name must be at least 3 characters long")
     private String name;
+
+    @NotBlank(message = "Mobile Number must not be blank")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
     private String mobileNum;
+
+    @Email
+    @NotBlank(message = "Please fill in an E-mail")
     private String email;
+
+    @NotBlank(message = "Please fill in a Subject")
     private String subject;
+
+    @NotBlank(message = "Please describe more on message")
     private String message;
 
 }
